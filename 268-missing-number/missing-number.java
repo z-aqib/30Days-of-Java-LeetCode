e4305592edaf2d87914c5b04a0edaf9b1efe265e
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-13 13:50:08 +0500

    Time: 0 ms (100.00%) | Memory: 45 MB (69.67%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        // so we know that the range of numbers is from 0 to the count of numbers n
+        // so our approach is that we find the total of the array [0 + 1 + 2 + 3 + ... + n] where n is the number of integers in n
+        // and then we subtract each value of nums[i] from the total of array
+        // the value left over is the missing number
+        int total = 0; // this is the total of the array
+        for(int i = 0; i <= nums.length; i++) { // count the length of n as well
+            total = total + i;
+        }
+        // subtract nums[i] from the total and return the missing number
+        for(int i = 0; i < nums.length; i++) {
+            total = total - nums[i];
+        }
+        return total;
+    }
+}

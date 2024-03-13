f837a73f8082519f1e621803a4f70aae6f30f35f
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-13 15:00:55 +0500

    Time: 6 ms (37.18%) | Memory: 44.1 MB (40.58%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
         // the approach is to make x a string, and check front back both. if both are equal, return true (yes its a palindrome), if at any point front does not equal to back, return false. 
+        if (x < 0) {
+            return false;
+        }
         String number = "" + x;
         for (int i = 0; i < number.length() / 2; i++) {
             if (number.charAt(i) != number.charAt(number.length() - i - 1)) {

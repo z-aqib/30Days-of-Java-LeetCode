f13e69f69335c54b9cdf53e0f347d6ee682c1ed7
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-13 14:51:25 +0500

    Time: 1 ms (99.85%) | Memory: 45.9 MB (59.04%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -4,8 +4,8 @@ class Solution {
         // this is tested using the example [7, 3, 5, 4, 5, 3, 4] where when we will do 7 ^ 3 ^ 5 ^ 4 ^ 5 ^ 3 ^ 4. XOR is exclusive OR which applies XOR gate to the binary form of two numbers. 
         // 7^3 = 111 ^ 011 = 100 ^ 101 (5) = 001 ^ 100 (4) = 101 ^ 101 (5) = 000 ^ 011 (3) = 011 ^ 100 (4) = 111 (7) where 7 is the correct answer (it is the single number). 
 
-        int xor_answer = nums[0];
-        for(int i = 1; i< nums.length; i++) {
+        int xor_answer = 0;
+        for(int i = 0; i < nums.length; i++) {
             xor_answer = xor_answer ^ nums[i];
         }
         return xor_answer;

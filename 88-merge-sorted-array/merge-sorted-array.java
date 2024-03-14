6acb18e8926e44a3551692e2dc461052bef0982a
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-15 00:54:10 +0500

    Time: 2 ms (7.63%) | Memory: 42 MB (60.49%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -7,17 +7,17 @@ class Solution {
         int temp;
         int n_x;
         while (m_counter < (m + n) && n_counter < n) {
-            n_x = n_counter;
-            while (n_x < (n - 1) && nums2[n_x] > nums2[n_x + 1]) {
-                temp = nums2[n_x];
-                nums2[n_x] = nums2[n_x + 1];
-                nums2[n_x++ + 1] = temp;
-            }
             if (m_counter < m) {
                 if (nums2[n_counter] < nums1[m_counter]) {
                     temp = nums1[m_counter];
                     nums1[m_counter++] = nums2[n_counter];
                     nums2[n_counter] = temp;
+                    n_x = n_counter;
+                    while (n_x < (n - 1) && nums2[n_x] > nums2[n_x + 1]) {
+                        temp = nums2[n_x];
+                        nums2[n_x] = nums2[n_x + 1];
+                        nums2[n_x++ + 1] = temp;
+                    }
                 } else
                     m_counter++;
             } else

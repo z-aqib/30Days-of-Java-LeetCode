e470e389f64d39bee95d5708b0ef4cbca55df8ef
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 01:26:22 +0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (31.87%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -5,13 +5,13 @@ class Solution {
         // array and get the middle. update minimum. then if the middle is greater than
         // the right side, move to that half, if it is lesser, move to that half. keep
         // computing until the first is greater than the last.
-        
+
         int left = 0;
         int right = nums.length - 1;
         int minimum = nums[0];  
         while (left <= right) {
             int mid = (left + right) / 2;
-            minimum = Math.min(minimum, nums[mid]);
+            minimum = Math.min(minimum, nums[mid]); // find the minimum value
             if (nums[mid] > nums[right]) {
                 left = mid + 1;
             } else {

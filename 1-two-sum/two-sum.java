63becebcc0af12465862b3d29c4cc1492b605807
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 08:04:10 +0500

    Time: 91 ms (5.10%) | Memory: 44.8 MB (60.86%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -2,14 +2,14 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         /*
          * so this can easily be done using two for loops, get each value and check if
-         * they add up to target. 
+         * they add up to target.
          */
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
-                if (i != j) {
-                    if (nums[i] + nums[j] == target) {
-                        return new int[] { i, j };
-                    }
+                if (i == j)
+                    continue;
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
                 }
             }
         }

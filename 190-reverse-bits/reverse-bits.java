c7f31ddeb507945a3a444f29ab5f1f6712eb11a6
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 07:29:19 +0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (10.46%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        /*
+         * so in this approach we take the reversed_n as 0, shift it left, add the last
+         * bit of n to reverse_n, then shift n right. we do this 32 times to make sure n
+         * is fully shifted for each of the 32 bits.
+         */
+        int answer = 0;
+        for (int i = 0; i < 32; i++) {
+            answer <<= 1;
+            answer = answer | (n & 1);
+            n >>= 1;
+        }
+        return answer;
+    }
+}

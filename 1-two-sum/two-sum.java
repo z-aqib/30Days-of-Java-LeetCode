85da852aaa22bc405295cc8b4817768bac5255f5
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 08:07:23 +0500

    Time: 0 ms (100.00%) | Memory: 45.1 MB (18.54%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -2,14 +2,15 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         /*
          * so this can easily be done using two for loops, get each value and check if
-         * they add up to target.
+         * they add up to target. but we need to check, what if i == j (the values we
+         * get are the same value?) then we should not sum them. but if we use an
+         * if-condition, it makes our time complexity 90ms. so we can apply that i
+         * starts from 1 and j starts from i, and it computes (j-i) and i.
          */
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i == j)
-                    j++;
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] + nums[j - i] == target) {
+                    return new int[] { j - i, j };
                 }
             }
         }

4645de2cbf501b32223f7358ab15e2f82a643564
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 09:04:30 +0500

    Time: 1 ms (77.87%) | Memory: 41.6 MB (45.56%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -8,9 +8,10 @@ class Solution {
          * string. then we get the next string and repeat.
          */
         String prefix = strs[0];
+        boolean matched;
         for (int i = 1; i < strs.length; i++) {
             String str_to_match = strs[i];
-            boolean matched = true;
+            matched = true;
             for (int j = 0; j < str_to_match.length() && j < prefix.length(); j++) {
                 if (str_to_match.charAt(j) != prefix.charAt(j)) {
                     prefix = prefix.substring(0, j);

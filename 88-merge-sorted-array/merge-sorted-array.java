d22b3484b2bf3b532c9cb39939adb7ce1e406332
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 06:41:51 +0500

    Time: 0 ms (100.00%) | Memory: 42.2 MB (43.29%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,27 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n == 0)
-            return;
-        int m_counter = 0;
-        int n_counter = 0;
-        int temp;
-        int n_x;
-        while (m_counter < (m + n) && n_counter < n) {
-            if (m_counter < m) {
-                if (nums2[n_counter] < nums1[m_counter]) {
-                    temp = nums1[m_counter];
-                    nums1[m_counter] = nums2[n_counter];
-                    nums2[n_counter] = temp;
-                    n_x = n_counter;
-                    while (n_x < (n - 1) && nums2[n_x] > nums2[n_x + 1]) {
-                        temp = nums2[n_x];
-                        nums2[n_x] = nums2[n_x + 1];
-                        nums2[n_x++ + 1] = temp;
-                    }
-                }
-            } else
-                nums1[m_counter] = nums2[n_counter++];
-            m_counter++;
+        // so if i start from front and merge from there, then it takes 2ms which is
+        // only 7% of other users (its taking too much time), this is because when we
+        // merge we have to sort multiple times the nums2 array. hence it is time
+        // complex. lets try it from back merging.
+        // so the approach is that we start a pointer of merged array from the last
+        // element and take elemnts from nums1 and nums2 (whichever is bigger)
+        // respectively. if one array has been completed, copy the second array's
+        // normally.
+        int m_pointer = m + n - 1;
+        while (m_pointer >= 0 && (n - 1) >= 0 && (m - 1) >= 0) {
+            if (nums1[m - 1] > nums2[n - 1]) {
+                nums1[m_pointer--] = nums1[m-- - 1];
+            } else {
+                nums1[m_pointer--] = nums2[n-- - 1];
+            }
         }
+        // if any elements from n are left, copy them into nums1
+        while ((n - 1) >= 0) {
+            nums1[m_pointer--] = nums2[n-- - 1];
+        }
+        // if any elemnts from m are left, then they already exist, no need to copy
     }
 }

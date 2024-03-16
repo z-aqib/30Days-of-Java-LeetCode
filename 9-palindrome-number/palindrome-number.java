d1c3f18774072148ef5b0a9789ed931fef1145d4
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 08:35:23 +0500

    Time: 4 ms (100.00%) | Memory: 44.3 MB (14.42%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        // in this approach we flip the digits of x and then compare if they are equal
-
-        // so if my value of x is negative, it can never be a palindrome. reject immediately. 
+        /*
+         * in this approach we flip the digits of x and then compare if they are equal
+         * so if my value of x is negative, it can never be a palindrome. reject
+         * immediately. then we will take a copy of x and take its last digit and
+         * increment it to reverse integer each time until x finished. then we will
+         * compare if they are equal
+         */
         if (x < 0) {
             return false;
         }
-
-        // now we will take a copy of x and take its last digit and increment it to reverse integer each time until x finished. then we will compare if they are equal
         int temp_x = x;
         int reverse_x = 0;
-
         while (temp_x >= 10) {
             int last_digit = temp_x % 10;
             reverse_x = (reverse_x * 10) + last_digit;
@@ -18,17 +19,11 @@ class Solution {
         }
         reverse_x = (reverse_x * 10) + temp_x;
         return reverse_x == x;
-
-
         /*
-        // the approach is to make x a string, and check front back both. if both are equal, return true (yes its a palindrome), if at any point front does not equal to back, return false. 
-        String number = "" + x;
-        for (int i = 0; i < number.length() / 2; i++) {
-            if (number.charAt(i) != number.charAt(number.length() - i - 1)) {
-                return false;
-            }
-        }
-        return true;
-        */
+         * another approach could be to compare each digit of x, front and back, if
+         * same, move front forward and back backward until they are equal or front is
+         * greater than back. if any time they are not same, reject. BUT this algorithm
+         * takes more time complexity.
+         */
     }
 }

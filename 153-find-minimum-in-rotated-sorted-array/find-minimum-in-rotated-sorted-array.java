7f0894e932a4fbb9faf542a75f6b53231ed48ba7
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 08:24:52 +0500

    Time: 0 ms (100.00%) | Memory: 41.7 MB (71.85%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,14 +1,15 @@
 class Solution {
     public int findMin(int[] nums) {
-        // so this approach is using O(logn) time complexity
-        // in this we did binary search. we started from the first and last index of
-        // array and get the middle. update minimum. then if the middle is greater than
-        // the right side, move to that half, if it is lesser, move to that half. keep
-        // computing until the first is greater than the last.
-
+        /*
+         * so this approach is using O(logn) time complexity
+         * in this we did binary search. we started from the first and last index of
+         * array and get the middle. update minimum. then if the middle is greater than
+         * the right side, move to that half, if it is lesser, move to that half. keep
+         * computing until the first is greater than the last.
+         */
         int left = 0;
         int right = nums.length - 1;
-        int minimum = nums[0];  
+        int minimum = nums[0];
         while (left <= right) {
             int mid = (left + right) / 2;
             minimum = Math.min(minimum, nums[mid]); // find the minimum value

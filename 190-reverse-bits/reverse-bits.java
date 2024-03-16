ed0bbdbf4dbd41e9923cc6cb22f10ebdbada3a38
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 07:35:01 +0500

    Time: 0 ms (100.00%) | Memory: 41.3 MB (95.52%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -2,9 +2,10 @@ public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         /*
-         * so in this approach we take the reversed_n as 0, shift it left, add the last
-         * bit of n to reverse_n, then shift n right. we do this 32 times to make sure n
-         * is fully shifted for each of the 32 bits.
+         * so in this approach we take the reversed_n as 0, shift it left (move all
+         * digits to the left and add a 0), add the last bit of n to reverse_n, then
+         * shift n right (move all digits to the right, the last one is dropped). we do
+         * this 32 times to make sure n is fully shifted for each of the 32 bits.
          * how do we add the last bit? using a bit wise unary OR operator (|) and the
          * bit wise AND operator (&) with 1
          */

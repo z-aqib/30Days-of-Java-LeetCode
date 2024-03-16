487c944c3b1b04988ac1860fea8daf0a432f17c5
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 07:30:05 +0500

    Time: 0 ms (100.00%) | Memory: 42 MB (16.48%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -5,6 +5,7 @@ public class Solution {
          * so in this approach we take the reversed_n as 0, shift it left, add the last
          * bit of n to reverse_n, then shift n right. we do this 32 times to make sure n
          * is fully shifted for each of the 32 bits.
+         * how do we add the last bit? using a bit wise OR operator (|)
          */
         int answer = 0;
         for (int i = 0; i < 32; i++) {

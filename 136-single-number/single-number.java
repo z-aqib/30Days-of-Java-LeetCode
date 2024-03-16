d8195bbd32a8cb636ba1536f2eb7d55e827d1074
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 08:14:36 +0500

    Time: 1 ms (99.85%) | Memory: 46.1 MB (35.21%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -8,7 +8,7 @@ class Solution {
          * binary form of two numbers.
          * 7^3 = 111 ^ 011 = 100 ^ 101 (5) = 001 ^ 100 (4) = 101 ^ 101 (5) = 000 ^ 011
          * (3) = 011 ^ 100 (4) = 111 (7) where 7 is the correct answer (it is the single
-         * number).
+         * number)
          */
         int xor_answer = 0;
         for (int i = 0; i < nums.length; i++) {

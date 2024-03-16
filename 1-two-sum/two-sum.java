db91875abb728f019bcbabc34bff2539e7c8aced
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 08:04:34 +0500

    Time: 118 ms (5.10%) | Memory: 45 MB (30.62%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -7,7 +7,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
                 if (i == j)
-                    continue;
+                    j++;
                 if (nums[i] + nums[j] == target) {
                     return new int[] { i, j };
                 }

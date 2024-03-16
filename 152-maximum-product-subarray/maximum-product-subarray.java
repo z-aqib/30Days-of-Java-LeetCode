bf9b2a6cfd4d9fb42ffa8074b4af0b6e09e6fb5c
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 08:17:06 +0500

    Time: 0 ms (100.00%) | Memory: 45.3 MB (7.74%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,9 +1,16 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        // so our approach is that we want the maximum subarray which can be of any length. our subarray will stop when we encounter a 0 as the entire product would be 0. so we compute a new subarray after that
-        int max_product = -10;
+        /*
+         * so our approach is that we want the maximum subarray which can be of any
+         * length. our subarray will stop when we encounter a 0 as the entire product
+         * would be 0. so we compute a new subarray after that
+         * so first we find the maximum product of the subarray going forwards from
+         * index 0, then we do the same thing going backwards from index nums.length-1.
+         * the maximum is taken of all.
+         */
+        int max_product = -10; // the smallest nums[i] value is -10
         int temp_product = 1;
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             temp_product = temp_product * nums[i];
             max_product = Math.max(max_product, Math.max(temp_product, nums[i]));
             if (nums[i] == 0) {
@@ -11,7 +18,7 @@ class Solution {
             }
         }
         temp_product = 1;
-        for(int i = nums.length -1; i >= 0; i--) {
+        for (int i = nums.length - 1; i >= 0; i--) {
             temp_product = temp_product * nums[i];
             max_product = Math.max(max_product, Math.max(temp_product, nums[i]));
             if (nums[i] == 0) {

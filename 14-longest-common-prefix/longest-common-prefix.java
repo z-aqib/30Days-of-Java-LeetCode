16d8e50ee9c99cac811486480102ba600842bc21
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-16 09:04:03 +0500

    Time: 1 ms (77.87%) | Memory: 41.2 MB (76.95%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,7 +1,14 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        /*
+         * so the approach is that prefix is the first string. now we get the second
+         * string, match each of its letters with prefix, if we find an unmatch, we make
+         * prefix till BEFORE that un-match. but if we do not find any un-matches AND
+         * the second string is smaller than the prefix, then prefix becomes the second
+         * string. then we get the next string and repeat.
+         */
         String prefix = strs[0];
-        for (int i = 1; i<strs.length; i++) {
+        for (int i = 1; i < strs.length; i++) {
             String str_to_match = strs[i];
             boolean matched = true;
             for (int j = 0; j < str_to_match.length() && j < prefix.length(); j++) {

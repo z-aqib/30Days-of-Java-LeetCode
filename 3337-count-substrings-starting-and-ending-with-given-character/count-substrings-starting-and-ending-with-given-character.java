422e65ce299c1e2d26bf0eb9abbc5046c7890db0
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-17 10:46:16 +0500

    Time: 1 ms (100.00%) | Memory: 44.8 MB (50.00%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.java
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.java
@@ -1,15 +1,18 @@
 class Solution {
     public long countSubstrings(String s, char c) {
-        long counter = 0;
-        int index_counter = 0;
-        for(int i=0; i<s.length(); i++) {
+        /*
+         * so the approach is that we first count how many character c there are in the
+         * string s, and then we use the sum of natural numbers formula n(n+1)/2 to
+         * calculate how many substrings there are. this formula is used because a
+         * pattern was forming (when c=3, substrings=6)(when c=4, substrings=10)(when
+         * c=5, substrings=15) hence pattern is 6,10,15.
+         */
+        long character_counter = 0;
+        for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == c) {
-                index_counter++; 
+                character_counter++;
             }
         }
-        for(int i = 0; i<index_counter; i++) {
-            counter+= (index_counter-i);
-        }
-        return counter;
+        return character_counter * (character_counter + 1) / 2;
     }
 }

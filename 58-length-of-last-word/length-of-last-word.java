bbb71864062e12a2504eff9e60488dd6aa8082c3
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-18 14:38:31 +0500

    Time: 0 ms (100.00%) | Memory: 41.6 MB (48.15%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -3,7 +3,8 @@ class Solution {
         /*
          * so one approach can be that to traverse from the start of the string and
          * count the length. but this takes too much time. a better approach is to
-         * traverse from the end of the string and count until a space is found.
+         * traverse from the end of the string and count until a space is found. remove
+         * all spaces using s.trim() built in function.
          */
         s = s.trim();
         int word_length = 0;

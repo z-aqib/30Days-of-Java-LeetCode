2dbf822098e00514ac5ab1d0e36b2d106c1c6daa
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-18 21:18:13 +0500

    Time: 1 ms (85.75%) | Memory: 41 MB (21.47%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -3,13 +3,11 @@ class Solution {
         long min_pointer = 1;
         long max_pointer = x;
         long middle_value;
-        long middle_square;
         while (min_pointer <= max_pointer) {
             middle_value = (max_pointer + min_pointer) / 2;
-            middle_square = middle_value * middle_value;
-            if (middle_square == x)
+            if (middle_value * middle_value == x)
                 return (int) middle_value;
-            else if (middle_square > x)
+            else if (middle_value * middle_value > x)
                 max_pointer = middle_value - 1;
             else
                 min_pointer = middle_value + 1;

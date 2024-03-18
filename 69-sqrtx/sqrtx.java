98b9ed396954677df99f90ebcaeca00b8f2e7627
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-18 21:25:48 +0500

    Time: 1 ms (85.75%) | Memory: 40.6 MB (81.32%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,15 @@
 class Solution {
     public int mySqrt(int x) {
+        /*
+         * so the approach is that we use binary search on finding the sqrt of x. what
+         * we do is that we start from range [1, x/2]. we do not use [1, x] as using x/2
+         * our iterations get lesser. we find the middle, check if its the sqrt(x), if
+         * yes, return middle, else if the middle's square is greater than x, then we
+         * move to the [1, middle] range as our middle is too high, but if the middle's
+         * square is lesser than x, then we move to the [middle, x/2] range as our
+         * middle is too low. we compute until our range gets nil. then we return the
+         * right side of the range (max_pointer) as it would be rounded up.
+         */
         long min_pointer = 1;
         long max_pointer = x;
         long middle_value;

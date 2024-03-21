55536580efcb6ac4daeaec63eaa51a6c9ae20863
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-21 10:44:22 +0500

    Time: 0 ms (100.00%) | Memory: 41.8 MB (63.18%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,5 +1,16 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        /*
+         * so the approach used is that we check each nums[i] and then see if its equal
+         * to the value we are looking for, we increase moving_length by 1 and intialise
+         * nums[i] as nums[i+1]. then we recheck nums[i] again, if it is again value,
+         * then increment moving_length again and intialise it as nums[i] as nums[i+2].
+         * now recheck nums[i] again and if it is not the value, then move i one ahead.
+         * now the next nums[i] is exmaple not the value so it is declared nums[i+2] as
+         * 2 values before the current i have been removed so we need to increment it
+         * two places. this continues until the end of the array, then the total no of
+         * elements - the number of elements removed is equal to the remaining elements.
+         */
         int moving_length = 0;
         for (int i = 0; i < nums.length;) {
             if (nums[i] == val)

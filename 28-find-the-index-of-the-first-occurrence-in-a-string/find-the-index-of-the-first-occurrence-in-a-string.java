52305710db819040146923bba51b291c30661e35
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-21 20:19:06 +0500

    Time: 0 ms (100.00%) | Memory: 41.8 MB (7.67%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,11 +1,16 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+        /*
+         * so in this approach we get each index of haystack and create a string from
+         * that index to index + needle.length(). if the string is equal to needle, it
+         * means haystack contains it, return that haystack index. it will always start
+         * checking for the first (zeroth) index hence it always gives first occurence
+         * of needle. if no index is found, return -1.
+         */
         for (int i = 0; i + needle.length() <= haystack.length(); i++) {
             if (haystack.substring(i, i + needle.length()).equals(needle) == true)
                 return i;
         }
-        if (haystack.equals(needle) == true)
-            return 0;
         return -1;
     }
 }

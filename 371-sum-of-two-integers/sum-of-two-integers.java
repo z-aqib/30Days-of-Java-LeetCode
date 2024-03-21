1e229f0c9f1011143aaac338d924bb88ffcd3d3a
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-21 10:23:08 +0500

    Time: 0 ms (100.00%) | Memory: 40.2 MB (48.64%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getSum(int a, int b) {
+        /*
+         * so in this approach we calculate the sum and carry of a and b and then re
+         * calculate it iteratively on the new values of a and b (a is sum and b is
+         * carry) and then continue calculating until b (carry) is 0.
+         * we calculate sum using XOR
+         * we calculate carry using (a AND b) and then we shift left this value by 1
+         */
+        int sum;
+        int carry;
+        while (b != 0) {
+            sum = a ^ b;
+            carry = (a & b) << 1;
+            a = sum;
+            b = carry;
+        }
+        return a;
+    }
+}

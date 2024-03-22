b909f34bbaad2801b0d05d190b7aca23bd0baf18
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-23 00:04:06 +0500

    Time: 0 ms (100.00%) | Memory: 40.2 MB (50.09%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,25 @@
 class Solution {
     public int fib(int n) {
+        /*
+         * one approach can be to make a recursive function where it returns fib(n-1) +
+         * fib(n-2) as fib(n) is the sum of its previous two numbers. but this can be
+         * slow as it recomputes each fib below each n multiple times repeatedly. to fix
+         * this, we introduce an array which saves each fib(n) so it is calculated only
+         * once. when fib(n) is re-called multiple times, it will just retreive the
+         * calculated value from the array.
+         */
+        int[] array = new int[n+1];
+        return fibonacci(n, array);
+    }
+
+    public int fibonacci(int n, int[] array) {
         if (n == 0)
             return 0;
-        else if (n == 1)
+        if (n == 1)
             return 1;
-        return fib(n - 1) + fib(n - 2);
+        if (array[n] != 0)
+            return array[n];
+        array[n] = fibonacci(n - 1, array) + fibonacci(n - 2, array);
+        return array[n];
     }
 }

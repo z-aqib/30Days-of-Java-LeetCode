f1c74a90101d0c36d152f764735870692391643f
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-03-23 15:33:40 +0500

    Time: 0 ms (100.00%) | Memory: 41.6 MB (43.73%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,6 +10,17 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        /*
+         * so the approach is that we first traverse the linked list from start to end
+         * and count how long it is. then we subtract how long it is from n to find
+         * which node we have to remove from the START. we subtract 1 as well as we
+         * start from 0. then we check, are we removing the first element (head)? if
+         * yes, remove it and return the next node after head. if not, start a pointer
+         * which moves one node ahead and incremenets itself. it keeps moving until we
+         * reach the removing element. now the loop breaks and now we are at the element
+         * where we have to remove the NEXT element. so the .next is equal to the
+         * .next.next. return head.
+         */
         ListNode duplicateHead = head;
         int pointer = 0;
         while (duplicateHead != null) {
@@ -17,14 +28,13 @@ class Solution {
             duplicateHead = duplicateHead.next;
         }
         n = pointer - n;
-        pointer = 0;
         if (n == 0)
             return head.next;
         else {
+            pointer = 0;
             duplicateHead = head;
-            while ((pointer++ ) != (n-1)) {
+            while (pointer++ != n-1)
                 duplicateHead = duplicateHead.next;
-            }
             // remove this element
             duplicateHead.next = duplicateHead.next.next;
             return head;

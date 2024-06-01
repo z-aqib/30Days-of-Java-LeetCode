16aa9022d92e376fa33ff06309bdd2071af90c56
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2024-06-01 16:39:58 +0500

    Time: 0 ms (100.00%) | Memory: 42.6 MB (84.30%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,6 +1,11 @@
 class Solution {
 
     public int searchInsert(int[] nums, int target) {
+        /*
+         * this is done by binary search. move first and last pointers accordingly and
+         * return the leftside pointer i.e. the first pointer so that if first is
+         * greater than last, that means it should have first position.
+         */
         int first = 0;
         int last = nums.length - 1;
         int middle;
